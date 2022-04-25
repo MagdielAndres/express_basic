8aef50803702f383c2a1acb11535edf0f432a7e2
MagdielAndres <magdiel.vampire@gmail.com>
2022-04-25 10:22:51 -0500

    Paso 6: Agregando nueva ruta para regresar un objeto

--- app.js
+++ app.js
@@ -22,3 +22,10 @@ app.listen(port, () => {
 app.get('/launchx',(req, res) => {
     res.send('Bienvenidos a launchx')
 })
+
+// Regresando un objeto
+// localhost:3000/explorersInNode
+app.get('/explorersInNode', (req, res) => {
+    const explorer = {name: "Explorer Magdiel Andrés", msg: "Hello"}
+    res.send(explorer)
+})
